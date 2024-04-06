c862790b35b976c561029c4d8387c49e52fab5c2
aravind2804 <136676935+aravind2804@users.noreply.github.com>
2024-04-06 10:37:59 +0530

    Add files via upload

---
+++ mail/WebForm1.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="mail.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Sent mail"></asp:Label>
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="click" OnClick="Button1_Click" /> 
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ mail/WebForm1.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace mail
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+                smtpClient.Port = 587; // or 465 for SSL
+                smtpClient.Credentials = new NetworkCredential("babua8405@gmail.com", "yehk mqfv bzxw wrmw");
+                smtpClient.EnableSsl = true;
+
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.From = new MailAddress("babua8405@gmail.com");
+                mailMessage.To.Add("aravi0428@gmail.com");
+                mailMessage.Subject = "Testing";
+                mailMessage.Body = "Handshake";
+
+                // Send the email
+                smtpClient.Send(mailMessage);
+
+                Label1.Text = "Email sent successfully!";
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+
+
+        }
+    }
+}
